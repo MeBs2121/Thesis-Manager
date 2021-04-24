fe627d8a7798815a0ab2bdb4b0405938c528d762
MeBs2121 <react2130@yahoo.co.jp>
2021-04-24 16:29:58 +0900

    delete images

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,16 +1,16 @@
 class Book < ApplicationRecord
   belongs_to :user
   has_many :notes
-  has_one_attached :image
+  # has_one_attached :image
 
   validates :title, presence:true
   validates :author, presence:true
 
-  validate :image_presence
-
-  def image_presence
-    unless image.attached?
-      errors.add(:image, 'must be attached')
-    end
-  end
+  # validate :image_presence
+  #
+  # def image_presence
+  #   unless image.attached?
+  #     errors.add(:image, 'must be attached')
+  #   end
+  # end
 end

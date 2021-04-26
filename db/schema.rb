72a1785482a91574cc1add47abe4db30eca487a2
MeBs2121 <react2130@yahoo.co.jp>
2021-04-26 18:55:24 +0900

    created model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_22_021436) do
+ActiveRecord::Schema.define(version: 2021_04_26_094633) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -81,9 +81,19 @@ ActiveRecord::Schema.define(version: 2021_04_22_021436) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "vocabularies", force: :cascade do |t|
+    t.string "word"
+    t.string "meaning"
+    t.integer "book_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["book_id"], name: "index_vocabularies_on_book_id"
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "books", "users"
   add_foreign_key "note_category_relations", "categories"
   add_foreign_key "note_category_relations", "notes"
   add_foreign_key "notes", "books"
+  add_foreign_key "vocabularies", "books"
 end

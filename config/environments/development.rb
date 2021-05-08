b73723b1d4537972730f0ee18780989638342853
MeBs2121 <react2130@yahoo.co.jp>
2021-05-08 17:44:31 +0900

    finished confirmable devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,4 +61,14 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => Rails.application.credentials.dig(:gmail, :address),
+    :password => Rails.application.credentials.dig(:gmail, :password),
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end

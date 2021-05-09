4bb841fad5dd52580362cd5d7f2a9a57f861741e
MeBs2121 <react2130@yahoo.co.jp>
2021-05-09 10:47:24 +0900

    cheanged scheme

--- db/schema.rb
+++ db/schema.rb
@@ -77,10 +77,6 @@ ActiveRecord::Schema.define(version: 2021_05_08_083228) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

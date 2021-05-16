b7945810ba047752aa3a0c0b2f7941c598f11f01
MeBs2121 <react2130@yahoo.co.jp>
2021-05-16 12:49:31 +0900

    note show edited

--- app/helpers/notes_helper.rb
+++ app/helpers/notes_helper.rb
@@ -1,2 +1,9 @@
 module NotesHelper
+  def fix_text(array)
+    # array.map!{|x| x=="" ? "\n\n" : x}
+    # array.join()
+
+    output = array.map{|x| x=="" ? "\n\n \n" : x}
+    output.join()
+  end
 end
